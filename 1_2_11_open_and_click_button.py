2f537b2836ecbab41cc0b0dfed345c394be61067
Michael K <mKomelin@list.ru>
2023-07-29 10:33:43 +0300

    fix: Changed the waiting time and added a comment - 1_2_11_open_and_click_button.py

--- 1_2_11_open_and_click_button.py
+++ 1_2_11_open_and_click_button.py
@@ -10,11 +10,11 @@ from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 
 # команда time.sleep устанавливает паузу в 3 секунд, чтобы мы успели увидеть, что происходит в браузере
-time.sleep(5)
+time.sleep(3)
 
 # Метод get сообщает браузеру, что нужно открыть сайт по указанной ссылке
 driver.get('https://suninjuly.github.io/text_input_task.html')
-time.sleep(5)
+time.sleep(3)
 
 # Метод find_element позволяет найти нужный элемент на сайте, указав путь к нему.
 # Метод принимает в качестве аргументов способ поиска и значение, по которому мы будем искать
@@ -23,14 +23,17 @@ textarea = driver.find_element(By.CSS_SELECTOR, '.textarea')
 
 # Напишем текст ответа в найденное поле
 textarea.send_keys('get()')
-time.sleep(5)
+time.sleep(3)
 
 # Найдем кнопку, которая отправляет введенное решение
 submit_button = driver.find_element(By.CSS_SELECTOR, '.submit-submission')
 
 # Скажем драйверу, что нужно нажать на кнопку. После этой команды мы должны увидеть сообщение о правильном ответе
 submit_button.click()
-time.sleep(5)
+time.sleep(3)
 
-# После выполнения всех действий мы должны не забыть закрыть окно браузера
+''' После выполнения всех действий мы должны не забыть закрыть окно браузера. Отличие между browser.close()
+    и browser.quit() - close() закрывает только текущее окно браузера, а quit() закрывает все окна, вкладки,
+    и процессы вебдрайвера, запущенные во время тестовой сессии.
+'''
 driver.quit()
