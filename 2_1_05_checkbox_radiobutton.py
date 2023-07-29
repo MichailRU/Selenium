32157f4bb50026d4d9434351b6670ec4dfdd496b
Michael K <mKomelin@list.ru>
2023-07-29 12:26:17 +0300

    feat(2_1_05_checkbox_radiobutton): Added a comment

--- 2_1_05_checkbox_radiobutton.py
+++ 2_1_05_checkbox_radiobutton.py
@@ -1,3 +1,34 @@
+'''
+Checkbox и Radiobutton создаются при помощи тега <input type='checkbox'> и <input type='radio'>
+Если checkbox или radiobutton выбран, то у элемента появится новый атрибут checked без значения.
+Radiobuttons объединяются в группу, где все элементы имеют одинаковые значения атрибута name,
+но разные значения атрибута value:
+<input type='radio' name='language' value='python' checked>
+<input type='radio' name='language' value='selenium'>
+
+Данные элементы лучше искать с помощью значения id или значения атрибута value.
+Чтобы снять/поставить галочку в элементе типа checkbox или выбрать опцию из группы radiobuttons,
+надо указать WebDriver метод поиска элемента и выполнить для найденного элемента метод click():
+option1 = browser.find_element(By.CSS_SELECTOR, "[value='python']")
+option1.click()
+
+Также вы можете увидеть тег label рядом с input. Этот тег используется, чтобы сделать кликабельным
+текст, который отображается рядом с флажком. Этот текст заключен внутри тега label. Элемент label
+связывается с элементом input с помощью атрибута for, в котором указывается значение атрибута id для элемента input:
+<div>
+  <input type='radio' id='python' name='language' checked>
+  <label for='python'>Python</label>
+</div>
+<div>
+  <input type='radio' id='java' name='language'>
+  <label for='java'>Java</label>
+</div>
+
+В этом случае можно также отметить нужный пункт с помощью WebDriver, выполнив метод click() на элементе label.
+option1 = browser.find_element(By.CSS_SELECTOR, "[for='java']")
+option1.click()
+'''
+
 import time
 from math import log, sin
 from selenium import webdriver
